00ee6bb380db141885a0db0d7c6e63909e4787cc
renatoova <81925895+renatoova@users.noreply.github.com>
2024-11-12 12:03:45 -0500

    Add files via upload

--- SemanaFina_Proyecto_Final/print_objects.cpp
+++ SemanaFina_Proyecto_Final/print_objects.cpp
@@ -1,4 +1,3 @@
-
 #include<GLFW/glfw3.h>
 #include<cmath>
 #define STB_IMAGE_IMPLEMENTATION // constante utilizada para implementar texturas
@@ -7,7 +6,7 @@
 
 using namespace std;
 
-GLuint texturaID[2];
+GLuint texturaID[4];
 
 GLuint loadTextureFile(const char* path) {
     // Cargar la imagen utilizando la librería stb_image
@@ -36,12 +35,10 @@ GLuint loadTextureFile(const char* path) {
     }
     else if (ch == 4) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-
     }
     stbi_image_free(data); // Liberamos la imagen de la memoria.
     return texturaID;
 }
-
 
 //Configurar su visualización, proyección en perspectiva
 void configFrustumParameter() {
@@ -58,74 +55,123 @@ void configFrustumParameter() {
     float left = -right;
     glFrustum(left, right, bottom, top, nearClip, farClip); // establece una matriz de proyección en perspectiva
     /*Este tipo de proyección es comúnmente utilizada en gráficos 3D para simular la profundidad*/
- 
+
     texturaID[0] = loadTextureFile("textures/textura.jpg");
     texturaID[1] = loadTextureFile("textures/cuerpo.jpg");
+    texturaID[2] = loadTextureFile("textures/denmin_fabric_02_diff_4k.jpg");
+    texturaID[3] = loadTextureFile("textures/cabeza.jpg");
 
+}
 
+void printVentanas() {
+
+    //Crear ventana Pared izquierda
+    glBindTexture(GL_TEXTURE_2D, texturaID[2]); // Cargar la textura de la ventana
+
+    glBegin(GL_QUADS);
+
+    // Ventana en la pared izquierda (ajusta las coordenadas según el tamaño y posición deseada)
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.25f, 0.0f, -0.2f);  // inferior izquierda de la ventana
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.25f, 0.0f, 0.2f);   // inferior derecha de la ventana
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.25f, 0.4f, 0.2f);   // superior derecha de la ventana
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.25f, 0.4f, -0.2f);  // superior izquierda de la ventana
+
+    glEnd();
+
+    // Crear ventana en la pared derecha
+    glBindTexture(GL_TEXTURE_2D, texturaID[2]); // Cargar la textura de la ventana
+
+    glBegin(GL_QUADS);
+
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(1.25f, 0.0f, -0.2f);   // inferior izquierda de la ventana
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(1.25f, 0.0f, 0.2f);    // inferior derecha de la ventana
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(1.25f, 0.4f, 0.2f);    // superior derecha de la ventana
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(1.25f, 0.4f, -0.2f);   // superior izquierda de la ventana
+
+    glEnd();
+}
+
+void printCuadro() {
+    // Dibujamos el cuadro en la pared frontal
+    glBindTexture(GL_TEXTURE_2D, texturaID[3]); // Cargar la textura para el cuadro
+
+    glBegin(GL_QUADS);
+    // Cuadro centrado en la pared frontal, ajustado al 50% de la superficie
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-0.416f, -0.06f, 0.5f);  // inferior izquierda del cuadro
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(0.416f, -0.06f, 0.5f);   // inferior derecha del cuadro
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(0.416f, 0.26f, 0.5f);    // superior derecha del cuadro
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-0.416f, 0.26f, 0.5f);   // superior izquierda del cuadro
+    glEnd();
 }
 
 void printSquad() {
-        
-        //Función para dibujar un estructura con textura
-        glEnable(GL_TEXTURE_2D); //habilitamos el uso de texturas
-        
-        glBindTexture(GL_TEXTURE_2D, texturaID[0]); // enlaza la presente textura (de la estructura)
-        // con la que hemos cargado, es decir, "texturaID"  
-        glBegin(GL_QUADS);
-        // Cara frontal
-        //glTexCoord2f(0.0f, 0.0f); glVertex3f(-0.5f, -0.5f, 0.5f);
-        //glTexCoord2f(1.0f, 0.0f); glVertex3f(0.5f, -0.5f, 0.5f);
-        //glTexCoord2f(1.0f, 1.0f); glVertex3f(0.5f, 0.5f, 0.5f);
-        //glTexCoord2f(0.0f, 1.0f); glVertex3f(-0.5f, 0.5f, 0.5f);
-   
-        // pared trasera
-        // glColor3f(0.0f, 1.0f, 0.0f); //verde
-        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.25, -0.5f, -0.5f);  // inferior izquierda
-        glTexCoord2f(1.0f, 0.0f); glVertex3f(1.25f, -0.5f, -0.5f);; // inferior derecha
-        glTexCoord2f(1.0f, 1.0f); glVertex3f(1.25f, 0.5f, -0.5f);;  // superior derecha
-        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.25f, 0.5f, -0.5f); // superior izquierda
-        
-        glEnd();
 
-        glBindTexture(GL_TEXTURE_2D, texturaID[0]); // enlaza la presente textura (de la estructura)
-        // con la que hemos cargado, es decir, "texturaID"  
-        glBegin(GL_QUADS);
-        // pared izquierda
-        // glColor3f(1.0f, 0.0f, 0.0f); // rojo
-        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.25f, -0.5f, -0.5f);
-        glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.25f, -0.5f, 0.5f);
-        glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.25f, 0.5f, 0.5f);
-        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.25f, 0.5f, -0.5f);
+    //Función para dibujar un estructura con textura
+    glEnable(GL_TEXTURE_2D); //habilitamos el uso de texturas
 
-        glEnd();
 
-        glBindTexture(GL_TEXTURE_2D, texturaID[0]); // enlaza la presente textura (de la estructura)
-        // con la que hemos cargado, es decir, "texturaID"  
-        glBegin(GL_QUADS);
-        // pared derecha
-        // glColor3f(1.0f, 0.0f, 0.0f); // rojo
-        glTexCoord2f(0.0f, 0.0f); glVertex3f(1.25f, -0.5f, -0.5f);
-        glTexCoord2f(1.0f, 0.0f); glVertex3f(1.25f, -0.5f, 0.5f);
-        glTexCoord2f(1.0f, 1.0f); glVertex3f(1.25f, 0.5f, 0.5f);
-        glTexCoord2f(0.0f, 1.0f); glVertex3f(1.25f, 0.5f, -0.5f);
 
-        glEnd();
+    glBindTexture(GL_TEXTURE_2D, texturaID[0]); // enlaza la presente textura (de la estructura)
+    // con la que hemos cargado, es decir, "texturaID"  
 
-        glBindTexture(GL_TEXTURE_2D, texturaID[1]); // enlaza la presente textura (de la estructura)
-        // con la que hemos cargado, es decir, "texturaID"  
-        glBegin(GL_QUADS);
-        // piso del establecimiento
-        // glColor3f(0.0f, 0.0f, 1.0f); // azul
-        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.25f, -0.5f, -0.5f);
-        glTexCoord2f(1.0f, 0.0f); glVertex3f(1.25f, -0.5f, -0.5f);
-        glTexCoord2f(1.0f, 1.0f); glVertex3f(1.25f, -0.5f, 0.5f);
-        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.25f, -0.5f, 0.5f);
 
-        glEnd();
-        //fin de la generación de la estructura
-        glDisable(GL_TEXTURE_2D); //deshabilitamos el uso de texturas para otros objetos
-        //si los hubiera
-   
+    glBegin(GL_QUADS);
+    // Cara frontal
+    //glTexCoord2f(0.0f, 0.0f); glVertex3f(-0.5f, -0.5f, 0.5f);
+    //glTexCoord2f(1.0f, 0.0f); glVertex3f(0.5f, -0.5f, 0.5f);
+    //glTexCoord2f(1.0f, 1.0f); glVertex3f(0.5f, 0.5f, 0.5f);
+    //glTexCoord2f(0.0f, 1.0f); glVertex3f(-0.5f, 0.5f, 0.5f);
+
+    // pared trasera
+    // glColor3f(0.0f, 1.0f, 0.0f); //verde
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.25, -0.5f, -0.5f);  // inferior izquierda
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(1.25f, -0.5f, -0.5f);; // inferior derecha
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(1.25f, 0.5f, -0.5f);  // superior derecha
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.25f, 0.5f, -0.5f); // superior izquierda
+
+    glEnd();
+
+    glBindTexture(GL_TEXTURE_2D, texturaID[0]); // enlaza la presente textura (de la estructura)
+    // con la que hemos cargado, es decir, "texturaID"  
+    glBegin(GL_QUADS);
+    // pared izquierda
+    // glColor3f(1.0f, 0.0f, 0.0f); // rojo
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.25f, -0.5f, -0.5f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.25f, -0.5f, 0.5f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.25f, 0.5f, 0.5f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.25f, 0.5f, -0.5f);
+
+    glEnd();
+
+    glBindTexture(GL_TEXTURE_2D, texturaID[0]); // enlaza la presente textura (de la estructura)
+    // con la que hemos cargado, es decir, "texturaID"  
+    glBegin(GL_QUADS);
+    // pared derecha
+    // glColor3f(1.0f, 0.0f, 0.0f); // rojo
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(1.25f, -0.5f, -0.5f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(1.25f, -0.5f, 0.5f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(1.25f, 0.5f, 0.5f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(1.25f, 0.5f, -0.5f);
+
+    glEnd();
+
+    glBindTexture(GL_TEXTURE_2D, texturaID[1]); // enlaza la presente textura (de la estructura)
+    // con la que hemos cargado, es decir, "texturaID"  
+    glBegin(GL_QUADS);
+    // piso del establecimiento
+    // glColor3f(0.0f, 0.0f, 1.0f); // azul
+    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.25f, -0.5f, -0.5f);
+    glTexCoord2f(1.0f, 0.0f); glVertex3f(1.25f, -0.5f, -0.5f);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(1.25f, -0.5f, 0.5f);
+    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.25f, -0.5f, 0.5f);
+
+    glEnd();
+    //fin de la generación de la estructura
+
+    printVentanas();
+    printCuadro();
+
+    glDisable(GL_TEXTURE_2D); //deshabilitamos el uso de texturas para otros objetos
+    //si los hubiera
+
 }
-
